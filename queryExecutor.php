e3b8989aa04e210175a35a6c9b9e3d10a84fe0aa
nirmansonawane3 <nirmansonawane3@gmail.com>
2017-02-23 10:33:35 +0530

    Add files via upload

---
+++ queryExecutor.php
@@ -0,0 +1,107 @@
+<?php
+//server connection script
+include 'dbconnect.php';
+
+//tag which is fetch from sender
+$tags=$_POST['tag'];
+
+//main logic
+switch ($tags)
+{
+case "createDatabase":
+
+    $DBName=$_POST['dbName'];
+    $sql = "CREATE DATABASE $DBName";
+    if ($conn->query($sql) === TRUE) {
+      echo "Database created successfully";
+    } else {
+      echo "Error creating database: " . $conn->error;
+    }
+    $conn->close();
+    break;
+
+case "selectDB":
+    $DBName=$_POST['dbName'];
+    if($conn->select_db($DBName))
+    {
+      echo "Database selected successfully..!";
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+
+case "createTable":
+    $DB_Name=$_POST['db_Name'];
+    $query_Stat=$_POST['Query'];
+    if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Table created successfully";
+      } else {
+          echo "Error creating table: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+
+case "DropTable":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Table dropped successfully";
+      } else {
+          echo "Error dropping table: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+	
+	
+case "DropDatabase":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Database dropped successfully";
+      } else {
+          echo "Error dropping Database: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database does not exist..!";
+    }
+    break;
+
+case "Alteraddcoloumn":
+
+  $DB_Name=$_POST['database_Name'];
+    $query_Stat=$_POST['Query'];
+    if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Coloumn created successfully";
+      } else {
+          echo "Error creating table: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+default:
+    echo "Nothing to show";
+}
+
+?>
